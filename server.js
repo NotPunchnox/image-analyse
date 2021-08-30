55e7a2de8bed638b285fd66c8b8245083c3c270d
NotPunchnox <74469788+NotPunchnox@users.noreply.github.com>
2021-08-30 19:18:50 +0200

    Update server.js

--- server.js
+++ server.js
@@ -4,23 +4,27 @@ const express = require("express"),
   cocoSsd = require("@tensorflow-models/coco-ssd"),
   tf = require("@tensorflow/tfjs-node")
 
-app.post("/object-detection", async (req, res) => {
+app.use(express.json())
 
+app.post("/", async (req, res) => {
+  
   const uri = String(req.body.image)
   
   function getImg(u) {
     return new Promise(async resolve => {
-      let r = await fetch(u);
-      resolve(r.buffer());
-    });
+      let r = await fetch(u)
+      resolve(r.buffer())
+    })
   }
 
   const a = await [cocoSsd.load(), await getImg(uri)];
 
   const img = tf.node.decodeImage(new Uint8Array(a[1]), 3);
 
-  
+  // Load the model.
   const model = await cocoSsd.load();
+
+  // Classify the image.
   const predictions = await model.detect(img);
 
   console.log(predictions)
