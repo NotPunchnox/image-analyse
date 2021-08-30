c85a103ee6c42b3a87e72843df8c9727499e6c29
NotPunchnox <74469788+NotPunchnox@users.noreply.github.com>
2021-08-30 17:07:20 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,34 +1,32 @@
 const express = require("express"),
   app = express(),
-  fetch = require('node-fetch')
+  fetch = require('node-fetch'),
+  cocoSsd = require("@tensorflow-models/coco-ssd"),
+  tf = require("@tensorflow/tfjs-node")
 
 app.post("/object-detection", async (req, res) => {
-    require("@tensorflow/tfjs-backend-cpu")
-    require("@tensorflow/tfjs-backend-webgl")
-    const cocoSsd = require("@tensorflow-models/coco-ssd")
-    const tf = require("@tensorflow/tfjs-node")
 
-    function getImg(u) {
-      return new Promise(async resolve => {
-        let r = await fetch(u)
-        resolve(r.buffer())
-      })
-    }
-    
-    const a = await [cocoSsd.load(), await getImg(req.query.uri)]
+  const uri = String(req.body.image)
+  
+  function getImg(u) {
+    return new Promise(async resolve => {
+      let r = await fetch(u);
+      resolve(r.buffer());
+    });
+  }
 
-    const img = tf.node.decodeImage(new Uint8Array(a[1]), 3)
+  const a = await [cocoSsd.load(), await getImg(uri)];
 
-    // Load the model.
-    const model = await cocoSsd.load()
+  const img = tf.node.decodeImage(new Uint8Array(a[1]), 3);
 
-    // Classify the image.
-    const predictions = await model.detect(img)
+  
+  const model = await cocoSsd.load();
+  const predictions = await model.detect(img);
 
-    if(!predictions) return res.status(403).json({ code: 403, message: 'error' })
-
-    return res.status(200).json({ code: 200, message: predictions})
+  console.log(predictions)
+  
+  res.status(200).json({code:200, message: predictions})
 
 })
 
-app.listen(process.env.PORT || 3000)
+app.listen(3000)
